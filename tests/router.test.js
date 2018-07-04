c28fa36c307eac97446918ece71d0893e7221e23
samundrak <samundrak@yahoo.com>
2018-07-04 22:47:51 +0545

    Add router tests

--- tests/router.test.js
+++ tests/router.test.js
@@ -1,1 +1,10 @@
-describe('Testing router', () => {});
+import Router from '../src/Router';
+
+describe('Testing router', () => {
+  test('Routing', () => {
+    const router = new Router();
+    router.get('/test', () => null, () => null);
+    expect(router.hasRoute('get', '/test')).toBe(true);
+    expect(router.getHandlers('get', '/test').length).toBe(1);
+  });
+});
